3bd9b4c3829cec74888177eb70392fc14f645e84
Miguel Angel Ramírez <miguel97angel@gmail.com>
2020-04-07 19:08:51 +0200

    Initial commit

---
+++ src/main/java/org/proyecto/GestionUrbanizacionApplication.java
@@ -0,0 +1,13 @@
+package org.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUrbanizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUrbanizacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/proyecto/GestionUrbanizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUrbanizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
