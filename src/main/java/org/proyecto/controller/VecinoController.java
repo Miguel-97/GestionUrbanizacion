50c2219d8a9564764403af4135276dfb78c134b6
apatxe <alexalcaraz1992@gmail.com>
2020-06-10 12:48:01 +0200

    Nuevas vistas

--- src/main/java/org/proyecto/controller/VecinoController.java
+++ src/main/java/org/proyecto/controller/VecinoController.java
@@ -172,7 +172,7 @@ public class VecinoController {
 		m.put("zonas", repoZona.findByCorresponde(urba));
 		m.put("view", "/vecino/homeUsuario");
 
-		return "/_t/frame";
+		return "/vecino/homeUsuario";
 	}
 
 	@GetMapping("/perfil")
